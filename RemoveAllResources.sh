324562f62f342272078ec9e1f259d70489655432
seanjasm <48635882+seanjasm@users.noreply.github.com>
2024-04-08 16:20:23 -0500

    Update RemoveAllResources.sh

--- RemoveAllResources.sh
+++ RemoveAllResources.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-kubectl delete ns stage dev0
+kubectl delete ns stage dev
 kubectl delete -f app/mssql/deployment.yaml --force --grace-period=0
 kubectl delete secret mssql --force --grace-period=0
 
