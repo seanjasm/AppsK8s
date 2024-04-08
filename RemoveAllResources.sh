805067e634254129053f05d424f1e0b2f726cf81
seanjasm <48635882+seanjasm@users.noreply.github.com>
2024-04-08 13:31:16 -0500

    Update RemoveAllResources.sh

--- RemoveAllResources.sh
+++ RemoveAllResources.sh
@@ -3,7 +3,8 @@
 
 
 kubectl delete namespace dev --force --grace-period=0
-kubectl delete namespace stage --force --grace-period=0
+kubectl delete -f app/nextstore/stage/deployment.yaml --force --grace-period=0
+kubectl delete -f app/nextstore/dev/deployment.yaml --force --grace-period=0
 
 kubectl delete secret mssql --force --grace-period=0
 kubectl delete -f app/mssql/deployment.yaml --force --grace-period=0
